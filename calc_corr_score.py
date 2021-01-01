80d53d735ed2dabb5745cbf0c1f7b14e554dd387
Amirhesam Salimnia <hesam.77s@gmail.com>
2021-01-01 18:14:59 +0330

    Create calc_corr_score.py

---
+++ calc_corr_score.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+
+def getPSNR(I1, I2):
+    s1 = cv.absdiff(I1, I2) #|I1 - I2|
+    s1 = np.float32(s1)     # cannot make a square on 8 bits
+    s1 = s1 * s1            # |I1 - I2|^2
+    sse = s1.sum()          # sum elements per channel
+    if sse <= 1e-10:        # sum channels
+        return 0            # for small values return zero
+    else:
+        shape = I1.shape
+        mse = 1.0 * sse / (shape[0] * shape[1] * shape[2])
+        psnr = 10.0 * np.log10((255 * 255) / mse)
+        return psnr
