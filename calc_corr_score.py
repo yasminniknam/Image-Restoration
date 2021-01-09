b22070b1b0a811c6480333c64df18a7225b98ea8
yasamin-niknam <41532584+yasamin-niknam@users.noreply.github.com>
2021-01-09 10:12:49 +0330

    Update calc_corr_score.py

--- calc_corr_score.py
+++ calc_corr_score.py
@@ -1,7 +1,13 @@
 import re 
 import os
+import sys
 import cv2 as cv
 import numpy as np
+import skimage.io as io
+
+sys.path.insert(0, '/content/photo_restoration/Global')
+import detection
+sys.path.remove('/content/photo_restoration/Global')
 
 def getPSNR(I1, I2):
     s1 = cv.absdiff(I1, I2) #|I1 - I2|
@@ -16,11 +22,71 @@ def getPSNR(I1, I2):
         psnr = 10.0 * np.log10((255 * 255) / mse)
         return psnr
 
+def irregular_hole_synthesize(img, new_img, mask):
+
+    img_np = np.array(img).astype("uint8")
+    new_img_np = np.array(new_img).astype("uint8")
+    mask_np = np.array(mask).astype("uint8")
+    mask_np = mask_np / 255
+    img_new = img_np * (1 - mask_np) + mask_np * new_img_np
+
+    hole_img = Image.fromarray(img_new.astype("uint8")).convert("RGB")
+
+    return hole_img
 
 address = "/content/photo_restoration/test_videos/recovered_frames/final_output/"
+final_address = "/content/photo_restoration/test_videos/recovered_frames/after_new_stage/"
 imagelist = os.listdir(address)
 imagelist.sort(key=lambda f: int(re.sub('\D', '', f)))
 # print(imagelist)
-image1 = cv.imread(address+'1.jpg')
-image2 = cv.imread(address+'2.jpg')
-print(getPSNR(image1, image2))
+# image1 = cv.imread(address+'140.jpg')
+# image2 = cv.imread(address+'101.jpg')
+# image3 = cv.imread(address+'105.jpg')
+# print(getPSNR(image1.copy(), image2.copy()))
+# print(getPSNR(image2.copy(), image3.copy()))
+
+image = [cv.imread(address+imagelist[i]) for i in range(len(imagelist))]
+
+stage_1_input_dir = ""
+mask_dir = ""
+gpu1 = 0
+input_opts_stage1 = ["--test_path", stage_1_input_dir, "--output_dir", mask_dir,
+                                    "--input_size", "scale_256", "--GPU", gpu1]
+
+_ , mask = detection.detection(input_opts_stage1, image, imagelist)
+
+if not os.path.exists(final_address):
+      os.makedirs(final_address)
+
+for i in range(len(image)):
+  prev = -1
+  next = 0
+  chosen = -1
+  # print("* frame "+str(i)+" *")
+  if i == 0:
+    next = getPSNR(image[i], image[i+1])
+    chosen = (next, i+1)
+    # print(getPSNR(image[i], image[i+1]))
+    # print("**")
+    # continue
+  elif i == len(image)-1:
+    prev = getPSNR(image[i], image[i-1])
+    chosen = (prev, i-1)
+    # print(getPSNR(image[i], image[i-1]))
+    # print("**")
+    # continue
+  else:
+    prev = getPSNR(image[i-1], image[i])
+    next = getPSNR(image[i], image[i+1])
+    if next > prev:
+      chosen = (next, i+1)
+    else:
+      chosen = (prev,i-1)
+    # print(getPSNR(image[i-1], image[i]))
+    # print(getPSNR(image[i], image[i+1]))
+    # print("**")
+    if chosen[0] > 19:
+      image[i] = irregular_hole_synthesize(image[i], image[chosen[1]], mask[i])
+    
+    io.imsave(os.path.join(final_address, imagelist[i]), img_as_ubyte(image[i] / 255.0))
+      
